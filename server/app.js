16001eb392be75c606deeb12bf541115ed5d4bf5
Sanskar-12 <sanskarv2004@gmail.com>
2024-04-25 21:59:50 +0530

    final commit

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { readFileSync } from "fs";
+import { createReadStream, readFileSync, statSync } from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -13,8 +13,36 @@ app.get("/", (req, res) => {
 });
 
 app.get("/video", (req, res) => {
-  const file = `${__dirname}/public/video.mp4`;
-  res.sendFile(file);
+  const filepath = `${__dirname}/public/video.mp4`;
+  const stat = statSync(filepath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (!range) {
+    res.status(400).send("Requires Range Headers");
+  }
+
+  const chunkSize = 10 ** 6;
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + chunkSize, fileSize);
+
+  const contentLength = end - start + 1;
+
+  const fileStream = createReadStream(filepath, {
+    start,
+    end,
+  });
+
+  fileStream.pipe(res);
+
+  const header = {
+    "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+
+  res.writeHead(206, header);
 });
 
 app.listen(4000, () => {
